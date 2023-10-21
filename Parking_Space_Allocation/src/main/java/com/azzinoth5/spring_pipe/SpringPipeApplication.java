7ba7409b3070157585da7aee272e04be158262cc
RouachedHoussemEddine <houssem.rouached@gmail.com>
2023-10-21 14:58:12 +0100

    Adding Microservices

---
+++ Parking_Space_Allocation/src/main/java/com/azzinoth5/spring_pipe/SpringPipeApplication.java
@@ -0,0 +1,13 @@
+package com.azzinoth5.spring_pipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipeApplication.class, args);
+	}
+
+}
